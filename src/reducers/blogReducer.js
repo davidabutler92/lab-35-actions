0f7454646dce8db2532792f37045c7c96672ba6f
davidabutler92 <davidabutler92@yahoo.com>
2021-02-17 15:44:35 -0800

    add reducer function

--- src/reducers/blogReducer.js
+++ src/reducers/blogReducer.js
@@ -1,7 +1,18 @@
+import { CREATE_BLOG } from '../actions/blogActions';
+
 export const initialState = {
   blogs: [],
 };
 
 export default function reducer(state, action) {
-  return state;
+  const { blogs } = state;
+  switch (action.type) {
+    case CREATE_BLOG:
+      return {
+        ...state,
+        blogs: [...blogs, action.payload],
+      };
+    default:
+      return state;
+  }
 }
