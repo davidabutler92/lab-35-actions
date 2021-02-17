fb6574893a269fecf88967a91343cc8cd79d4320
davidabutler92 <davidabutler92@yahoo.com>
2021-02-17 15:45:27 -0800

    add custom hook useSelector to return blogs

--- src/state/BlogProvider.js
+++ src/state/BlogProvider.js
@@ -1,13 +1,24 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
+import reducer, { initialState } from '../reducers/blogReducer';
 
 const BlogContext = createContext(null);
 
-export const BlogProvider = (BlogProvider = ({ children }) => {
-  const [state, dispatch] = useReducer();
+export const BlogProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <BlogContext.Provider value={{ state, dispatch }}>
       {children}
     </BlogContext.Provider>
   );
-});
+};
+
+export const useDispatch = () => {
+  const { dispatch } = useContext(BlogContext);
+  return dispatch;
+};
+
+export const useSelector = (selector) => {
+  const { state } = useContext(BlogContext);
+  return selector(state);
+};
